56591cf5375d4f8873841fabeb4b77803cda6b40
yeickjacson <yeickjacson.15@gmail.com>
2024-05-28 09:37:06 -0500

    All Created + estetica

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 const myCarouselElement = document.querySelector('#carouselExampleAutoplaying')
+const cardOpacity = document.getElementsByClassName('card')
+
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 1500,
   touch: false
 })
+
+cardOpacity.addEvenListener(click, e =>{ cardOpacity.style.opacity="1";})
